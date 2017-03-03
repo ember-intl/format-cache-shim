ea6333cf78e3f5e11bf56b2d9d26832ef7ce4f1f
Jason Mitchell <jason.mitchell.w@gmail.com>
2017-03-02 22:35:46 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 /* jshint node: true */
 'use strict';
 
-var path = require('path');
-var mergeTrees = require('broccoli-merge-trees');
-var Funnel = require('broccoli-funnel');
+const path = require('path');
+const funnel = require('broccoli-funnel');
+const mergeTrees = require('broccoli-merge-trees');
 
-var formatCachePath = path.dirname(require.resolve('intl-format-cache'));
+const sourcePath = path.join(path.dirname(require.resolve('intl-format-cache')), 'src');
 
 module.exports = {
   name: 'intl-format-cache',
 
-  treeForAddon: function(tree) {
-    var formatCacheTree = new Funnel(path.join(formatCachePath, 'src'));
-    var trees = mergeTrees([formatCacheTree, tree], { overwrite: true });
+  treeForAddon(tree) {
+    let formatCacheTree = funnel(sourcePath);
+    let trees = mergeTrees([formatCacheTree, tree], { overwrite: true });
 
     return this._super.treeForAddon.call(this, trees);
   }
