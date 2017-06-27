44d8ea2b06df810ae7decb79874db6c935576442
Jason Mitchell <jason.mitchell.w@gmail.com>
2017-06-27 02:30:57 -0700

    Use unwatchedDir to prevent ember-cli from setting up a watcher on intl-format-cache node path

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 const path = require('path');
 const funnel = require('broccoli-funnel');
 const mergeTrees = require('broccoli-merge-trees');
+const UnwatchedDir = require('broccoli-source').UnwatchedDir;
 
 const sourcePath = path.join(path.dirname(require.resolve('intl-format-cache')), 'src');
 
@@ -11,7 +12,7 @@ module.exports = {
   name: 'intl-format-cache',
 
   treeForAddon(tree) {
-    let formatCacheTree = funnel(sourcePath);
+    let formatCacheTree = funnel(new UnwatchedDir(sourcePath));
     let trees = mergeTrees([formatCacheTree, tree], { overwrite: true });
 
     return this._super.treeForAddon.call(this, trees);
